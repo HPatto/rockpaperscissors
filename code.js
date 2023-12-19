dc568a68275e4f18c31ce7f8d14d209fc54b3192
HPatto <hpatto97@gmail.com>
2023-12-19 15:05:17 -0300

    Complete full functionality for SPR

--- code.js
+++ code.js
@@ -60,12 +60,12 @@ function gameResult(userString, computerString) {
 
     if (userIndex === computerIndex) {
         // Computer wins.
-        loseString = loseString + computerString + " beats " + userString;
-        return [false, loseString];
+        // loseString = loseString + computerString + " beats " + userString;
+        return false; //, loseString];
     } else {
         // User wins.
-        winString = winString + userString + " beats " + computerString;
-        return [true, (winString)];
+        // winString = winString + userString + " beats " + computerString;
+        return true; //, (winString)];
     }
 }
 
@@ -102,42 +102,75 @@ function getRandomInt(value1, value2) {
 function playRound(userChoice) {
 
     let resultFound = false;
-    let final;
 
     while (!resultFound) {
-        console.log("Getting info:");
-        // let userTurn = getUserChoice();
+        // Get the inputs for the round
         let userTurn = userChoice;
         let compTurn = getComputerChoice();
 
-        console.log(userTurn);
-        console.log(compTurn);
-
+        // If it is a tie, the content is discarded
         if (!gameIsTied(userTurn, compTurn)) {
-            final = gameResult(userTurn, compTurn);
+            // Check if the user won
+            let userWin = gameResult(userTurn, compTurn);
+
+            // Get the table row elements with the inputs
+            let userRow = createAndPopulateTdElement(userTurn, userWin);
+            let compRow = createAndPopulateTdElement(compTurn, !userWin);
+
+            // Get the objects holding the current score
+            let userScore = document.querySelector(".user-score");
+            let compScore = document.querySelector(".computer-score");
+
+            // Increment the winner's tally
+            if (userWin) {
+                incrementCount(userScore);
+            } else {
+                incrementCount(compScore);
+            }
+
+            /*
+            Will need some code that turns one of the two td
+            elements into bold text.
+            */
+
+            // New row populated and added to the page.
+            let newRow = createAndPopulateTrElement(userRow, compRow); 
+            const resultsTable = document.querySelector(".results-list");
+            resultsTable.appendChild(newRow);
+
+            // Prevents re-trying the round due to a tie.
             resultFound = true;
         }
     }
-    return final;
+    return checkForResult();
 }
 
 // Switch statement on the container div.
 function handleClick(e) {
     let selectedButton = e.target.id;
     let sanitizedInput = selectedButton.toUpperCase();
+    let gameOver;
 
     switch (sanitizedInput) {
         case 'ROCK':
-            console.log("Vibe");
-            playRound("ROCK");
+            gameOver = playRound("ROCK");
+            if (gameOver) {
+                endOfGame();
+            }
             break;
 
         case 'PAPER':
-            playRound("PAPER");
+            gameOver = playRound("PAPER");
+            if (gameOver) {
+                endOfGame();
+            }
             break;
         
         case 'SCISSORS':
-            playRound("SCISSORS");
+            gameOver = playRound("SCISSORS");
+            if (gameOver) {
+                endOfGame();
+            }
             break;
     }
 }
@@ -155,29 +188,64 @@ document.addEventListener('DOMContentLoaded', function () {
 
 })
 
-// function game() {
-//     /*
-//     Play a best-of-x game of RPS!
-//     No functionality to terminate the game early if a win / loss is confirmed early.
-//     */
+// Create and populate td element
+function createAndPopulateTdElement(text, winningElement) {
+    // Create the new td element.
+    let tdElemenet = document.createElement('td');
 
-//     let numRounds = 5; // Odd numbers required for this!
-//     let userWins = 0;
-//     let round;
+    // Populate the element with the text.
+    tdElemenet.textContent = text;
 
-//     for (let i = 0; i < numRounds; i++) {
-//         round = playRound()
-//         console.log(round[1]);
-//         console.log("\n");
+    if (winningElement) {
+        tdElemenet.classList.add("td-bold");
+    }
 
-//         if (round[0]) {
-//             userWins++;
-//         }
-//     }
+    return tdElemenet;
+}
 
-//     if (userWins > numRounds / 2) {
-//         console.log("You won!");
-//     } else {
-//         console.log("You lost!");
-//     }
-// }
+// Create and populate tr element
+function createAndPopulateTrElement(td1, td2) {
+    // Create the new tr element.
+    let trElemenet = document.createElement('tr');
+
+    // Add the td's to the row element.
+    trElemenet.appendChild(td1);
+    trElemenet.appendChild(td2);
+    
+    return trElemenet;
+}
+
+// Check if someone has won
+function checkForResult() {
+    let winningScore = 5;
+
+    let userScore = parseInt(document.querySelector(".user-score").textContent);
+    let compScore = parseInt(document.querySelector(".computer-score").textContent);
+
+    if (userScore === winningScore || compScore === winningScore) {
+        return true;
+    }
+
+    return false;
+}
+
+// A function to increment the object's value by 1
+function incrementCount(object) {
+    let currentScore = parseInt(object.textContent);
+    let newScore = currentScore + 1;
+
+    object.textContent = newScore;
+}
+
+// A clean-up function to prevent further "hands"
+function endOfGame() {
+    
+    // Remove the listening element.
+    const containerDiv = document.querySelector(".options");
+    containerDiv.removeEventListener('click', handleClick);
+
+    // Perhaps display some text. "Refresh to play again!"
+    setTimeout(function () {
+        alert("Refresh to play again!");
+    }, 3000);
+}
